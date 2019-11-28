8a10762bf271ba4b8e8ec793a0f8bae234b284b4
Daniil Pavlenko <6151779@johnabbottcollege.net>
2019-11-28 18:25:22 -0500

    added function

---
+++ Class4DB/GetAgeByName.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[GetAgeByName]
+(
+	@Name varchar(50)
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @result int;
+
+	-- Add the T-SQL statements to compute the return value here
+	SELECT @result = Age
+	FROM Student
+	WHERE FirstName = @Name;
+	IF (@result IS NULL)   
+        SET @result = 'Nothing is found';  
+    
+	-- Return the result of the function
+	RETURN @result;
+END
--- Class4DB/Script.PostDeployment1.sql
+++ Class4DB/Script.PostDeployment1.sql
@@ -10,17 +10,26 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
+IF not exists(select 1  from Student)
+BEGIN
 INSERT INTO Student VALUES('Daniil', 'Pavlenko', 31, GETDATE(), null)
 INSERT INTO Student VALUES('Mr', 'Propper', 160, GETDATE(), null)
 INSERT INTO Student VALUES('John', 'Doe', 25, GETDATE(), null)
+END
 
+IF not exists(select 1  from Course)
+BEGIN
 INSERT INTO Course VALUES('Java', 180, GETDATE(), null)
 INSERT INTO Course VALUES('DataBase', 210, GETDATE(), null)
 INSERT INTO Course VALUES('JavaScript', 150, GETDATE(), null)
+END
 
+IF not exists(select 1  from Student_Course)
+BEGIN
 INSERT INTO Student_Course VALUES(1, 1)
 INSERT INTO Student_Course VALUES(1, 2)
 INSERT INTO Student_Course VALUES(2, 1)
 INSERT INTO Student_Course VALUES(2, 3)
 INSERT INTO Student_Course VALUES(3, 2)
 INSERT INTO Student_Course VALUES(3, 3)
+END
